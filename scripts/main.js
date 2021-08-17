5adc8ade44cdfdcc3277b5c0c4a52f9274908476
hmnpark <59078900+hmnpark@users.noreply.github.com>
2021-08-17 16:29:42 -0700

    refactor and style changes

--- scripts/main.js
+++ scripts/main.js
@@ -1,39 +1,40 @@
-const numImages = 49;
-let myArray = [];
 
-
+// shuffles the given array
 function shuffle(array) {
   array.sort(() => Math.random() - 0.5);
 }
 
-
-function getRandInt(max)
-{
-    return Math.floor(Math.random() * max) + 1;
+// return random int in [1, max]
+function getRandInt(max) {
+  return Math.floor(Math.random() * max) + 1;
 }
 
-
-function resetArray(myArray, size)
-{
-    for (let i = 1; i < size + 1; i++)
-    {
-        myArray[i - 1] = i;
+// fills array with ints from 1 to size, and shuffles it
+function resetArray(array, size) {
+  for (let i = 0; i < size; i++) {
+    let imageSrc = `images/image (${(i + 1).toString(10)}).jpg`;
+    if (array[i] != imageSrc) {
+      array.push(imageSrc);
     }
-    shuffle(myArray);
+  }
+  shuffle(array);
 }
 
+// change main image on click
+(function() {
+  const numImages = 49;
+  const myArray = [];
+  const myImage = document.querySelector('#main-image');
 
-let myImage = document.querySelector('img');
-myImage.onclick = function ()
-{
-    if (myArray.length === 0)
-    {
-        resetArray(myArray, numImages)
-        console.log('reset array')
+  function changeImage() {
+    if (myArray.length === 0) {
+      resetArray(myArray, numImages);
+      console.log('array reset');
     }
     console.log(myArray.length);
 
-    let currImageNum = myArray.pop();
-    let newImageSrc = 'images/image (' + currImageNum.toString(10) + ').jpg';
-    myImage.setAttribute('src', newImageSrc)
-}
+    myImage.setAttribute('src', myArray.pop());
+  }
+
+  myImage.onclick = () => void changeImage();
+})();
